0b91d51e885581c5150a566fc4a53a405f9123aa
snake <370696614@qq.com>
2024-07-31 00:00:22 +0800

    发布1.0.2

--- pisces-mybatis-plus-starter/src/main/java/io/github/yxsnake/pisces/mybatis/plus/configuration/CustomTenantConfiguration.java
+++ pisces-mybatis-plus-starter/src/main/java/io/github/yxsnake/pisces/mybatis/plus/configuration/CustomTenantConfiguration.java
@@ -1,21 +1,21 @@
 package io.github.yxsnake.pisces.mybatis.plus.configuration;
 
+import io.github.yxsnake.pisces.mybatis.plus.configuration.properties.MybatisPlusExtProperties;
 import lombok.RequiredArgsConstructor;
-import io.github.yxsnake.pisces.mybatis.plus.configuration.properties.TenantProperties;
 import io.github.yxsnake.pisces.mybatis.plus.handler.CustomTenantHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@Import(TenantProperties.class)
+@Import(MybatisPlusExtProperties.class)
 @Configuration
 @RequiredArgsConstructor
 public class CustomTenantConfiguration {
 
-    private final TenantProperties tenantProperties;
+    private final MybatisPlusExtProperties mybatisPlusExtProperties;
 
     @Bean
     public CustomTenantHandler customTenantHandler(){
-        return new CustomTenantHandler(tenantProperties);
+        return new CustomTenantHandler(mybatisPlusExtProperties);
     }
 }
